b9092673f6c8af179b8d83515e1d6a7b5b108786
Her19mo61so! <meritxellhermoso@gmail.com>
2021-04-23 14:43:46 +0200

    versió final minim 1 EA

---
+++ frontend/src/app/services/grupoinvestigacion.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { GrupoInvestigacion } from '../models/grupoinvestigacion'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GrupoinvestigacionService {
+
+  constructor(private http: HttpClient) { }
+
+  //obtenir tots els grups
+  getGrupos(): Observable<GrupoInvestigacion[]>{
+    return this.http.get<GrupoInvestigacion[]>(environment.apiURL + "/gruposinvestigacion");
+  }
+
+  //obtenir un grup
+  getGrupo(id: String): Observable<GrupoInvestigacion>{
+    return this.http.get<GrupoInvestigacion>(environment.apiURL + "/gruposinvestigacion/" + id);
+  }
+
+  //afegir un grup
+  addGrupo(nuevogrupo: GrupoInvestigacion): Observable<any>{
+    return this.http.post(environment.apiURL + '/gruposinvestigacion/new', nuevogrupo);
+  }
+
+  //modificar un grup
+  modificarGrupo(grupomodificado: GrupoInvestigacion, id: String): Observable<any>{
+    return this.http.put(environment.apiURL + "/gruposinvestigacion/update/" + id, grupomodificado);
+  }
+}
